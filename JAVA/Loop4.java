f19260279956745302661e5d0a30f9fb6715468a
MOHAMMED INSAF P <112387526+mohammedinsafpust@users.noreply.github.com>
2022-09-03 17:34:00 +0530

    Add files via upload

---
+++ JAVA/Loop4.java
@@ -0,0 +1,22 @@
+package com.ust;
+
+//Java Program to display numbers from 1 to 5
+
+import java.util.Scanner;
+
+//Program to find the sum of natural numbers from 1 to 100
+
+class Loop4 {
+	public static void main(String[] args) {
+		
+		int i=1,n=5;
+		
+	///do...while loop from 1 to 5
+	do {
+		System.out.println(i);
+		i++;
+		
+	}while(i<=n);
+	}
+
+}
