27e7ae4a31972b19e7aa966801698f880ea2a3b5
MOHAMMED INSAF P <112387526+mohammedinsafpust@users.noreply.github.com>
2022-09-05 09:30:16 +0530

    Add files via upload

---
+++ JAVA/Loop3.java
@@ -0,0 +1,14 @@
+package com.ust;
+
+class Loop3 {
+	public static void main(String[] args) {
+		//declare variables
+		int i=1,n=5;
+		//while loop from 1 to 5
+		while(i<=n) {
+			System.out.println(i);
+			i++;
+		}
+	}
+
+}
