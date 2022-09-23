a1d2d9a1b7e04b0342c2f4fbe5d0e7bb3bb3f762
MOHAMMED INSAF P <112387526+mohammedinsafpust@users.noreply.github.com>
2022-09-23 10:08:54 +0530

    Add files via upload

---
+++ JAVA/Synchronisation1.java
@@ -0,0 +1,19 @@
+package Synchronisation;
+
+public class Synchronisation1 extends Thread {
+public static void main(String[] args) {
+	Synchronisation1 t1=new Synchronisation1();
+	t1.start();
+	Thread t2=new Thread();
+	t2.start();
+	
+	
+}
+public void run() {
+	synchronized(this){
+		System.out.println("is synchronized");
+	}
+	System.out.println("Thread is Started");
+	System.gc();
+}
+}
